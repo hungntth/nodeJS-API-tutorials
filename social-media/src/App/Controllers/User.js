0a79cae20104f0f90a6f244fe70fd0f2958418e9
Nguyễn Thái Hưng <hungnt@techasians.com>
2022-05-29 16:34:03 +0700

    follow & unfollow

--- social-media/src/App/Controllers/User.js
+++ social-media/src/App/Controllers/User.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcrypt")
 
 // UPDATE USER
 const updateUser = async (req, res) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 req.body.password = bcrypt.hashSync(req.body.password, 10);
@@ -29,7 +29,7 @@ const updateUser = async (req, res) => {
 // DELETE USER
 
 const deleteUser = async (req, res) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         try {
             const user = await User.findByIdAndDelete(req.params.id)
             return res.status(200).json(`Account ${user.username} has been deleted`)
@@ -55,4 +55,47 @@ const getUserByID = async (req, res) => {
     }
 }
 
-module.exports = { updateUser, deleteUser, getUserByID }
+//follow a user
+const followUser = async (req, res) => {
+    if (req.body.userId != req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.params.id } });
+                res.status(200).json("user has been followed");
+            } else {
+                res.status(403).json("you allready follow this user");
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        return res.status.json("You cant follow yourself")
+    }
+}
+
+//unfollow
+
+const unfollowUser = (req, res) => {
+    if (req.body.userId !== req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            const currentUser = await User.findById(req.body.userId);
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.params.id } });
+                res.status(200).json("user has been unfollowed");
+            } else {
+                res.status(403).json("you dont follow this user");
+            }
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("you cant unfollow yourself");
+    }
+}
+
+module.exports = { updateUser, deleteUser, getUserByID, followUser, unfollowUser }
--- social-media/src/Routes/users.js
+++ social-media/src/Routes/users.js
@@ -6,5 +6,7 @@ const user = require("./../App/Controllers/User")
 router.put("/:id", user.updateUser)
 router.delete("/:id", user.deleteUser)
 router.get("/:id", user.getUserByID)
+router.put("/:id/follow", user.followUser)
+router.put("/:id/unfollow", user.unfollowUser)
 
 module.exports = router
